43dd028154fb7ee13cb573239696cdd98fdcabed
vita01 <vitalina27@gmail.com>
2023-09-25 08:05:11 +0200

    Upload your notebook to a public respository on GitHub

---
+++ _DataScienceEcosystem.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Data Science Tools and Ecosystem</h1>
+
+# <p>In this notebook, Data Science Tools and Ecosystem are summarized.</p>
+
+# <b>Objectives:</b>
+#     <ul>
+#   <li>Python</li>
+#   <li>Scala</li>
+#   <li>R</li>
+# </ul>
+
+# <p>Some of the popular languages that Data Scientists use are:</p>
+#     <ol type="1">
+#   <li>R</li>
+#   <li>Python</li>
+#   <li>Java </li>
+#   <li>Scala </li>
+# </ol>  
+
+# <p>Some of the commonly used libraries used by Data Scientists include:</p>
+#     <ol type="1">
+#   <li>TensorFlow</li>
+#   <li>SciPy</li>
+#   <li>Pandas</li>
+# </ol>
+
+# <table>
+#   <tr>
+#     <th>Data Science Tools</th>
+#     
+#   </tr>
+#   <tr>
+#     <td>Jupyter Notebook</td>
+#     
+#   </tr>
+#   <tr>
+#     <td>PyCharm</td>
+#     
+#   </tr>
+# <tr>
+#     <td>Visual Studio Code</td>
+#     
+#   </tr>
+# </table>
+
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python</h3>
+
+# In[1]:
+
+
+(3*4)+5 #This a simple arithmetic expression to mutiply then add integers
+
+
+# In[8]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+a=200/60
+print(str(a)+" hours")
+
+
+# <h2>Author</h2>
+# <p>Kumar Viktoriia</p>
+
+# In[ ]:
+
+
+
+
